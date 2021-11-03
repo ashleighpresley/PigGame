5007f3d89134b0a4eafa11fdebdd73877d8a6257
ashleighpresley <ashleighpresley1@gmail.com>
2021-11-03 15:02:23 -0500

    Roll & Hold Dice & Winner functionality

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 "use strict";
 
 //Selecting elements
+const player0 = document.querySelector(".player--0");
+const player1 = document.querySelector(".player--1");
 const score0 = document.querySelector("#score--0");
 const score1 = document.querySelector("#score--1");
+const current0 = document.querySelector("#current--0");
+const current1 = document.querySelector("#current--1");
 const dice = document.querySelector(".dice");
 const btnNewGame = document.querySelector(".btn--new");
 const btnRollDice = document.querySelector(".btn--roll");
@@ -12,10 +16,56 @@ const btnHoldDice = document.querySelector(".btn--hold");
 score0.textContent = 0;
 score1.textContent = 0;
 dice.classList.add("hide");
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+let playing = true;
+
+//function for switching players
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0.classList.toggle("player--active");
+  player1.classList.toggle("player--active");
+};
 
 //Rolling dice functionality
 btnRollDice.addEventListener("click", function () {
-  const diceImg = Math.trunc(Math.random() * 6) + 1;
-  dice.classList.remove("hide");
-  dice.src = `dice-${diceImg}.png`;
+  if (playing) {
+    const diceNumber = Math.trunc(Math.random() * 6) + 1;
+    dice.classList.remove("hide");
+    dice.src = `dice-${diceNumber}.png`;
+
+    if (diceNumber !== 1) {
+      currentScore += diceNumber;
+
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+btnHoldDice.addEventListener("click", function () {
+  //when hold button is clicked, add current score to players score
+  if (playing) {
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    if (scores[activePlayer] >= 100) {
+      playing = false;
+      dice.classList.add("hide");
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add(`player--winner`);
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove(`player--active`);
+    } else {
+      switchPlayer();
+    }
+  }
 });
